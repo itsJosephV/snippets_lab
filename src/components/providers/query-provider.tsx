9c641eca44a17bf784a350fe5f7581c04c6b1518
Joseph <jvp.2703@gmail.com>
2025-03-08 00:41:23 +0000

    feat: simplify query client initialization in RQueryProvider by removing unnecessary useState hook

--- src/components/providers/query-provider.tsx
+++ src/components/providers/query-provider.tsx
@@ -1,13 +1,10 @@
 "use client";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {useState} from "react";
+import {QueryClientProvider} from "@tanstack/react-query";
 
 import {getQueryClient} from "@/lib/get-query-client";
 
 function RQueryProvider({children}: {children: React.ReactNode}) {
-  const [queryClient] = useState(() => getQueryClient());
-
-  // const queryClient = getQueryClient();
+  const queryClient = getQueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
